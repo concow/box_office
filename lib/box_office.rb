110557d2f83d76d98717dc2a288098105b05fd15
Connor Cowan <ccowan003@comcast.net>
2019-07-25 22:39:44 +0000

    modified

--- lib/box_office.rb
+++ lib/box_office.rb
@@ -1,5 +1,7 @@
+#THIS FILE REQUIRES ALL MY FILES
 require "box_office/version"
-
+require_relative './'
+require_relative './box_office/cli'
 module BoxOffice
   class Error < StandardError; end
   # Your code goes here...
